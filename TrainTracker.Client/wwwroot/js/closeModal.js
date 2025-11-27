a58a57f3d65a85ca1294fb55d0eb24874a6e3cc0
arsenii.main <eqspertars@gmail.com>
2025-11-27 18:12:51 +0200

    Add dockerfiles

--- TrainTracker.Client/wwwroot/js/closeModal.js
+++
@@ -1,6 +0,0 @@
-function closeModal(modalName){
-    let modalEl = document.getElementById(modalName);
-    let modal = bootstrap.Modal.getInstance(modalEl);
-    if (!modal) modal = new bootstrap.Modal(modalEl);
-    modal.hide();
-}
--- TrainTracker.Client/wwwroot/js/playSound.js
+++
@@ -1,6 +0,0 @@
-function playSound() {
-    const audio = new Audio('/sound/notification.wav');
-    audio.volume = 0.1;       
-    audio.currentTime = 0;    
-    audio.play().catch(err => console.log('Ошибка воспроизведения:', err));
-}
