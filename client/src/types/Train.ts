b94e891c11e21f933da6c764ebae834f19c78767
Arsenii Main <eqspertars@gmail.com>
2025-12-25 00:15:30 +0200

    fix:app files structure

--- client/src/types/Train.ts
+++
@@ -1,9 +0,0 @@
-export interface Train {
-  id: number;
-  name: string;
-  number: number;
-  delayTime: number;
-  updateTime : number;
-  nextStation: { title: string };
-  incidents: string[];
-}
